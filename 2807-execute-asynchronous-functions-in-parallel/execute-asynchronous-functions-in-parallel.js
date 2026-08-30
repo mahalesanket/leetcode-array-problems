5047729c30899fff7c239244f274089f753302f1
Sanket Mahale <132744685+mahalesanket@users.noreply.github.com>
2026-08-30 12:45:51 +0530

    Time: 55 ms (89.96%) | Memory: 54.6 MB (46.13%) - LeetSync

---
+++ 2807-execute-asynchronous-functions-in-parallel/execute-asynchronous-functions-in-parallel.js
@@ -0,0 +1,30 @@
+/**
+ * @param {Array<Function>} functions
+ * @return {Promise<any[]>}
+ */
+var promiseAll = function(functions) {
+    return new Promise((resolve, reject) => {
+        const results = [];
+        let completed = 0;
+
+        if (functions.length === 0) {
+            resolve(results);
+            return;
+        }
+
+        functions.forEach((fn, index) => {
+            fn()
+                .then(value => {
+                    results[index] = value;
+                    completed++;
+
+                    if (completed === functions.length) {
+                        resolve(results);
+                    }
+                })
+                .catch(error => {
+                    reject(error);
+                });
+        });
+    });
+};
